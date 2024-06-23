38a301ce96a5a9159773cdf1d8a66c4a58e47834
prashant-atla <prashantatla@gmail.com>
2024-06-23 15:04:14 +0200

    first test for attributes check

---
+++ aws/attributesexample.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0440d3b780d96b29d"
+  instance_type = "t2.micro"
+}
