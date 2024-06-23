67a2689cd36b8646dfd4ab0222c05e814fff3c2a
prashant-atla <prashantatla@gmail.com>
2024-06-23 15:41:26 +0200

    more guidlines

--- aws/attributesexample.tf
+++ aws/attributesexample.tf
@@ -13,7 +13,11 @@ resource "aws_eip" "lb" {
 output "public-ip" {
   value = aws_eip.lb.public_ip
 }
+# if attribute is not specified then all the attributes under it are displayed
 
+output "public_new_ip" {
+  value = aws_eip.lb
+}
 resource "aws_instance" "web" {
   ami           = "ami-0440d3b780d96b29d"
   instance_type = "t2.micro"
